fece73f4003b8c7d97bd90f3e65b2aedcb006271
rxrt8 <michal25kos@gmail.com>
2020-04-29 19:37:46 +0200

    add eureka and zuul

--- c-application/src/main/java/pl/agh/customers/application/CustomersMSApplication.java
+++ c-application/src/main/java/pl/agh/customers/application/CustomersMSApplication.java
@@ -3,6 +3,7 @@ package pl.agh.customers.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -14,6 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 )
 @EntityScan("pl.agh.customers")
 @EnableJpaRepositories("pl.agh.customers")
+@EnableEurekaClient
 public class CustomersMSApplication {
 
     public static void main(String[] args) {
